17aac083bc673180125749a3d16687f117adf5f9
Wolkensteine <bokaj.bb22@gmail.com>
2023-01-01 12:00:45 +0100

    Adding Admin.py

--- Help.py
+++ Help.py
@@ -3,31 +3,54 @@ import datetime
 
 
 async def help_command(ctx, argument):
-    embed = discord.Embed(
-        title="WolkenBot - Help",
-        description="!help => help\n"
-                    "!friend => Answer with a random sentence - good luck :)\n"
-                    "!vote + vote duration in seconds => You can create a voting system (example: '!vote 60"
-                    "' to make in vote with a duration of one minute) [default duration: 60 seconds - "
-                    "maximal duration is 600 seconds]\n"
-                    "!info => informations about the bot\n"
-                    "!pin + (message content) => Will pin a custom Message [!Attention!: This will resend "
-                    "the Message as an embed, which might corrupt files attached to the message!]\n"
-                    "!math => Has some cool math functions implemented. Run !math.help to see all the "
-                    "commands of !math\n"
-                    "!gif + search term => Sends you a gif found with your search term\n"
-                    "!play. + term => plays a sound specified by the term. Get help with !play.help\n"
-                    "!g => Sends you a random Frog death message.\n"
-                    "!rate + text => Lets you rate sth easily\n"
-                    "!randomname => returns a random name\n"
-                    "!dadjoke => sends you a dad joke (feature requested by binMalKurzImWald)\n"
-                    "!admin.help => help you if you have the 'Admin'-Role",
-        colour=0xff8c1a,
-        url="https://Github.com/Wolkensteine/WolkenBot2.0",
-        timestamp=datetime.datetime.utcnow()
-    )
-    embed.set_footer(text="Message send by WolkenBot 2.0 created by Wolkensteine",
-                     icon_url="https://raw.githubusercontent.com/Wolkensteine/Wolkensteine/main/"
-                              "WolkensteineIcon.png")
+    if argument == "math":
+        print()
+    elif argument == "admin":
+        embed = discord.Embed(
+            title="Some help for you my Admin friend!",
+            description="You need to have a role named admin or an other role with privileged access.\n"
+                        "'/admin rights role' <right> <role> * n\n"
+                        "<right>s:\n"
+                        "accessall => grant access to all commands for a role\n"
+                        "mediumaccess => grant access to most commands for a role\n"
+                        "noaccess => deny access to all commands for a role\n"
+                        "'/admin rights command' <right> <command>\n"
+                        "<command>'s: /<command>\n"
+                        "The rights are the same as above.\n"
+                        "This will set a required permission level for a command.",
+            colour=0xff8c1a,
+            url="https://Github.com/Wolkensteine/WolkenBot2.0",
+            timestamp=datetime.datetime.utcnow()
+        )
+        embed.set_footer(text="Message send by WolkenBot 2.0 created by Wolkensteine",
+                         icon_url="https://raw.githubusercontent.com/Wolkensteine/Wolkensteine/main/"
+                                  "WolkensteineIcon.png")
+    else:
+        embed = discord.Embed(
+            title="WolkenBot - Help",
+            description="/help => help\n"
+                        "/friend => Answer with a random sentence - good luck :)\n"
+                        "/vote + vote duration in seconds => You can create a voting system (example: '/vote 60"
+                        "' to make in vote with a duration of one minute) [default duration: 60 seconds - "
+                        "maximal duration is 600 seconds]\n"
+                        "/info => informations about the bot\n"
+                        "/pin + (message content) => Will pin a custom Message [!Attention!: This will resend "
+                        "the Message as an embed, which might corrupt files attached to the message!]\n"
+                        "/math => Has some cool math functions implemented. Run '/help math' to see all the "
+                        "commands of /math\n"
+                        "/gif + search term => Sends you a gif found with your search term\n"
+                        "/play. + term => plays a sound specified by the term. Get help with !play.help\n"
+                        "/g => Sends you a random Frog death message.\n"
+                        "/rate + text => Lets you rate sth easily\n"
+                        "/randomname => returns a random name\n"
+                        "/dadjoke => sends you a dad joke (feature requested by binMalKurzImWald)\n"
+                        "/help admin => help you if you have the 'Admin'-Role",
+            colour=0xff8c1a,
+            url="https://Github.com/Wolkensteine/WolkenBot2.0",
+            timestamp=datetime.datetime.utcnow()
+        )
+        embed.set_footer(text="Message send by WolkenBot 2.0 created by Wolkensteine",
+                         icon_url="https://raw.githubusercontent.com/Wolkensteine/Wolkensteine/main/"
+                                  "WolkensteineIcon.png")
 
     await ctx.respond(embed=embed)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # imports
 import discord
-from discord.ext import commands
 import Help
+import admin
 
 # Bot setup
 intents = discord.Intents.all()
@@ -11,6 +11,47 @@ bot = discord.Bot(intents=intents, help_command=None)
 f = open("./Config/keys.txt")
 content = f.read().splitlines()
 discord_secret = content[0]
+
+# MyClient class
+class MyClient(discord.Client):
+    vote_channel = None
+    vote_creator = ""
+    votes_per_theme = []
+    voting_themes = []
+    vote_running = False
+    vote_duration = 30
+
+    # rights that might be granted
+    # if a role is not in one of the categories it will default to mediumaccess
+    access_all = []
+    medium_access = []
+    no_access = []
+    # Array with the server names to see which level must be loaded
+    servers = []
+    # Arrays for the needed rights for a specific command
+    # 0 => access all
+    # 1 => medium access
+    # 2 => no access
+    hello_command = []
+    friend_command = []
+    vote_command = []
+    info_command = []
+    pin_command = []
+    math_command = []
+    bot_command = []
+    gif_command = []
+    play_command = []
+    g_command = []
+    rate_command = []
+    random_name_command = []
+    dad_joke_command = []
+    mute_commands = []
+    fish_master_command = []
+    do_not_annoy_command = []
+    # Admin roles
+    admin_roles = []
+    # announcements
+    announcement_channels = []
 
 
 # events
@@ -47,4 +88,5 @@ async def hello(ctx, name: str = None):
 
 
 # Run bot
+admin.load_settings()
 bot.run(discord_secret)
