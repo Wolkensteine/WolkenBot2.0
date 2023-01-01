0444132509ed76e434cfbcb6dbebccc89dd3035b
Wolkensteine <bokaj.bb22@gmail.com>
2023-01-01 13:46:46 +0100

    Math rewrite

--- MathCommands.py
+++ MathCommands.py
@@ -2,33 +2,32 @@ import discord
 import datetime
 
 
-async def Math_command(message):
-    if message.content.lower().replace("!math.", "").startswith("root"):
-        inputs = message.content.lower().replace("!math.square ", "").split(" ")
-        temp_text = "The " + inputs[1] + ". root of " + inputs[2] + " is: " + \
-                    str(float(inputs[2]) ** 1 / float(inputs[1]))
-        embed = discord.Embed(
-            title="Math.Root",
-            description=temp_text,
-            colour=0xcc33ff,
-            url="https://Github.com/Wolkensteine/WolkenBot",
-            timestamp=datetime.datetime.utcnow()
-        )
-        embed.set_footer(text="Message send by WolkenBot made by Wolkensteine",
-                         icon_url="https://raw.githubusercontent.com/Wolkensteine/Wolkensteine/main/"
-                                  "WolkensteineIcon.png")
-        await message.channel.send(embed=embed)
-    elif message.content.lower().replace("!math.", "").startswith("square"):
-        inputs = message.content.lower().replace("!math.square ", "").split("^")
-        temp_text = inputs[0] + "^" + inputs[1] + " = " + str(float(inputs[0]) ** float(inputs[1]))
-        embed = discord.Embed(
-            title="Math.Square",
-            description=temp_text,
-            color=0xcc33ff,
-            url="https://Github.com/Wolkensteine/WolkenBot",
-            timestamp=datetime.datetime.utcnow()
-        )
-        embed.set_footer(text="Message send by WolkenBot made by Wolkensteine",
-                         icon_url="https://raw.githubusercontent.com/Wolkensteine/Wolkensteine/main/"
-                                  "WolkensteineIcon.png")
-        await message.channel.send(embed=embed)
+async def math_root(message, num1, num2):
+    temp_text = "The " + num1 + ". root of " + num2 + " is: " + \
+                str(float(num1) ** 1 / float(num2))
+    embed = discord.Embed(
+        title="Math.Root",
+        description=temp_text,
+        colour=0xcc33ff,
+        url="https://Github.com/Wolkensteine/WolkenBot2.0",
+        timestamp=datetime.datetime.utcnow()
+    )
+    embed.set_footer(text="Message send by WolkenBot made by Wolkensteine",
+                     icon_url="https://raw.githubusercontent.com/Wolkensteine/Wolkensteine/main/"
+                              "WolkensteineIcon.png")
+    await message.channel.send(embed=embed)
+
+
+async def math_square(message, num1, num2):
+    temp_text = num1 + "^" + num2 + " = " + str(float(num1) ** float(num2))
+    embed = discord.Embed(
+        title="Math.Square",
+        description=temp_text,
+        color=0xcc33ff,
+        url="https://Github.com/Wolkensteine/WolkenBot2.0",
+        timestamp=datetime.datetime.utcnow()
+    )
+    embed.set_footer(text="Message send by WolkenBot made by Wolkensteine",
+                     icon_url="https://raw.githubusercontent.com/Wolkensteine/Wolkensteine/main/"
+                              "WolkensteineIcon.png")
+    await message.channel.send(embed=embed)
